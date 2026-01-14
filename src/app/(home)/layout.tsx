daaf7e34f0c9ae9c3028ea9c5046f9e7da3ec783
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2026-01-14 15:34:32 +0700

    add Footer

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,17 +1,21 @@
 import { HomeLayout } from "fumadocs-ui/layouts/home";
+import Footer from "@/components/Footer";
 import { baseOptions } from "@/lib/layout.shared";
 
 export default function Layout({ children }: LayoutProps<"/">) {
+    const sharedOptions = baseOptions();
     return (
         <HomeLayout
-            {...baseOptions()}
+            {...sharedOptions}
             links={[
                 {
                     text: "Documentation",
                     url: "/docs",
                 },
+                ...(sharedOptions.links ?? []),
             ]}>
             {children}
+            <Footer />
         </HomeLayout>
     );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+        <footer className="py-5 text-fd-muted-foreground">
+            <p className="text-center">Released under the MIT License.</p>
+            <p className="text-center">Copyright © 2026 Toby Nguyen</p>
+        </footer>
+    );
+}
