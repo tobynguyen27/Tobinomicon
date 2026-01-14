0cb8ae2ce633630d652c05db2b668baac2571ce6
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2026-01-14 15:02:23 +0700

    add Discord server invite link

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -2,5 +2,16 @@ import { HomeLayout } from "fumadocs-ui/layouts/home";
 import { baseOptions } from "@/lib/layout.shared";
 
 export default function Layout({ children }: LayoutProps<"/">) {
-    return <HomeLayout {...baseOptions()}>{children}</HomeLayout>;
+    return (
+        <HomeLayout
+            {...baseOptions()}
+            links={[
+                {
+                    text: "Documentation",
+                    url: "/docs",
+                },
+            ]}>
+            {children}
+        </HomeLayout>
+    );
 }
--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -1,9 +1,19 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import DiscordIcon from "@/components/brands/DiscordIcon";
 
 export function baseOptions(): BaseLayoutProps {
     return {
         nav: {
-            title: "My App",
+            title: "Tobinomicon",
         },
+        githubUrl: "https://github.com/tobynguyen27/Tobinomicon",
+        links: [
+            {
+                type: "icon",
+                icon: <DiscordIcon />,
+                text: "Discord",
+                url: "https://discord.gg/nPz39wet3p",
+            },
+        ],
     };
 }
